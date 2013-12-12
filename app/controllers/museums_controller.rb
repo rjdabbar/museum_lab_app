d50d363c57a0654a73f499e12fa682df792a49ab
RJ Dabbar <rjdabbar@gmail.com>
2013-12-12 17:28:10 -0500

    finished museum app

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -1,10 +1,21 @@
 class MuseumsController < ApplicationController
+  
   def index
     @museums = Museum.all
   end
 
-
   def show
     @museum = Museum.find params[:id]
   end
+
+  def new
+    @museum = Museum.new
+  end
+
+  def create
+    safe_museum = params.require(:museum).permit(:name)
+    @museum = Museum.create safe_museum
+    redirect_to @museum
+  end
+
 end
--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -7,4 +7,17 @@ class PaintingsController < ApplicationController
     @paintings = Painting.all
   end
 
+  def new
+    @painting = Painting.new
+    @artists = Artist.all
+    @museums = Museum.all
+  end
+
+  def create
+    safe_painting = params.require(:painting).permit(:title, :image_url, :artist_id, :museum_id)
+    @painting = Painting.create safe_painting
+    redirect_to @painting
+  end
+
+
 end
